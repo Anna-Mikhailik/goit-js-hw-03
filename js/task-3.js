1dd936187adc45957c820c0489ffee0d3f663bc9
annplay <messageforannolay@gmail.com>
2024-12-15 23:42:32 +0200

    003

--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ function filterArray(numbers, value) {
   const suitableNambers = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > value) {
-      result.push(numbers[i]);
+      suitableNambers.push(numbers[i]);
     }
   }
-  return result;
+  return suitableNambers;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
